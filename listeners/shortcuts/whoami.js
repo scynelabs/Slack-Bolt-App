f916ad26ea1bffc28323ea4002104c7ee6ec71b0
Muhammad Bilal X Nazir <bilal.nazir@scyne.com.au>
2024-06-28 13:19:36 +1000

    shortcut test

--- listeners/shortcuts/whoami.js
+++ listeners/shortcuts/whoami.js
@@ -16,7 +16,7 @@ const whoamiCallback = async ({ shortcut, ack, client, context }) => {
             // Call the views.open method using one of the built-in WebClients
             const whoamiBlocksView = whoAmIResponse(conn.instanceUrl, currentuser.username)
             console.log('whoamiBlocksView ==>', whoamiBlocksView)
-            
+
             await client.views.open({
                 trigger_id: shortcut.trigger_id,
                 view: whoamiBlocksView//whoAmIResponse(conn.instanceUrl, currentuser.username)
@@ -36,4 +36,51 @@ const whoamiCallback = async ({ shortcut, ack, client, context }) => {
     }
 };
 
-module.exports = { whoamiCallback };
+const whoamiCallback2 =  async ({ shortcut, ack, client, logger }) => {
+
+    try {
+      // Acknowledge shortcut request
+      await ack();
+  
+      // Call the views.open method using one of the built-in WebClients
+      const result = await client.views.open({
+        trigger_id: shortcut.trigger_id,
+        view: {
+          type: "modal",
+          title: {
+            type: "plain_text",
+            text: "My App"
+          },
+          close: {
+            type: "plain_text",
+            text: "Close"
+          },
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "About the simplest modal you could conceive of :smile:\n\nMaybe <https://api.slack.com/reference/block-kit/interactive-components|*make the modal interactive*> or <https://api.slack.com/surfaces/modals/using#modifying|*learn more advanced modal use cases*>."
+              }
+            },
+            {
+              type: "context",
+              elements: [
+                {
+                  type: "mrkdwn",
+                  text: "Psssst this modal was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+                }
+              ]
+            }
+          ]
+        }
+      });
+  
+      logger.info(result);
+    }
+    catch (error) {
+      logger.error(error);
+    }
+}
+
+module.exports = { whoamiCallback: whoamiCallback2 };

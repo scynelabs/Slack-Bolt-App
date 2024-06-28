1c5f72e586e6d1820b98dff903113f9d4c2b52a5
Muhammad Bilal X Nazir <bilal.nazir@scyne.com.au>
2024-06-28 15:21:43 +1000

    added commands, update type

--- listeners/commands/index.js
+++ listeners/commands/index.js
@@ -4,8 +4,69 @@ const { injuredWorkerView } = require('./injuredWorker');
 const { captureNotes } = require('./captureNotes');
 const { carePlanView } = require('./carePlan');
 
+
+const injuredWorkerCommand = async ({ ack, body, client, logger }) => {
+    // Acknowledge the command request
+    await ack();
+  
+    try {
+      // Call views.open with the built-in client
+      const result = await client.views.open({
+        // Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id: body.trigger_id,
+        // View payload
+        view: injuredWorkerView
+
+        });
+        logger.info(result);
+    }
+    catch (error) {
+        logger.error(error);
+    }
+}
+
+const carePlanViewCommand = async ({ ack, body, client, logger }) => {
+    // Acknowledge the command request
+    await ack();
+  
+    try {
+      // Call views.open with the built-in client
+      const result = await client.views.open({
+        // Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id: body.trigger_id,
+        // View payload
+        view: carePlanView
+
+        });
+        logger.info(result);
+    }
+    catch (error) {
+        logger.error(error);
+    }
+}
+
+const captureNotesCommand = async ({ ack, body, client, logger }) => {
+    // Acknowledge the command request
+    await ack();
+  
+    try {
+      // Call views.open with the built-in client
+      const result = await client.views.open({
+        // Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id: body.trigger_id,
+        // View payload
+        view: captureNotes
+
+        });
+        logger.info(result);
+    }
+    catch (error) {
+        logger.error(error);
+    }
+}
+
 module.exports.register = (app) => {
-    app.command('/view_injured_worker', injuredWorkerView);
-    app.command('/view_care_plan', carePlanView);
-    app.command('/capture_notes', captureNotes);
+    app.command('/view_injured_worker', injuredWorkerCommand);
+    app.command('/view_care_plan', carePlanViewCommand);
+    app.command('/capture_notes', captureNotesCommand);
 };

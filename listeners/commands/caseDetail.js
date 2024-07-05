b874b8eb3d00d9f9cb918078ab476c1bcc16c1a7
Muhammad Bilal X Nazir <bilal.nazir@scyne.com.au>
2024-07-05 17:36:17 +1000

    case data

--- listeners/commands/caseDetail.js
+++ listeners/commands/caseDetail.js
@@ -1,6 +1,7 @@
     //             `SELECT Id, Work_Type_Group__r.Name, CaseNumber, Status, Licence_Type__c, Subject, Priority, Application_Date__c, Type, Account.Name FROM Case WHERE id='500Hu00002AzUVyIAN'`
 // eslint-disable
-const caseDetailView = async (case => {
+const caseDetailsView = async (caseInfo => {
+
 
     return {
         "type": "modal",
@@ -20,11 +21,11 @@ const caseDetailView = async (case => {
                 "fields": [
                     {
                         "type": "mrkdwn",
-                        "text": `*Case number:*\n${case.caseNumber}`
+                        "text": `*Case number:*\n${caseInfo.caseNumber}`
                     },
                     {
                         "type": "mrkdwn",
-                        "text": `*Subject:*\n${case.Subject}`
+                        "text": `*Subject:*\n${caseInfo.Subject}`
                     }
                 ]
             },
@@ -33,11 +34,11 @@ const caseDetailView = async (case => {
                 "fields": [
                     {
                         "type": "mrkdwn",
-                        "text": `*Plan Type:*\n${case.Licence_Type__c}`
+                        "text": `*Plan Type:*\n${caseInfo.Licence_Type__c}`
                     },
                     {
                         "type": "mrkdwn",
-                        "text": `*Contact Mobile:*\n${case.Application_Date__c}`
+                        "text": `*Contact Mobile:*\n${caseInfo.Application_Date__c}`
                     }
                 ]
             },
@@ -46,7 +47,7 @@ const caseDetailView = async (case => {
                 "fields": [
                     {
                         "type": "mrkdwn",
-                        "text": `*Employer:*\n${case.Account.Name}`
+                        "text": `*Employer:*\n${caseInfo.Account.Name}`
                     }
                 ]
             }

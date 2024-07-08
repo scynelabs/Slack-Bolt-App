ccb4a839aa17f7ff0e694ba7286aa51f7150b522
Muhammad Bilal X Nazir <bilal.nazir@scyne.com.au>
2024-07-08 16:52:25 +1000

    cases query update

--- salesforce/query/cases.js
+++ salesforce/query/cases.js
@@ -16,7 +16,7 @@ const queryAllCases = async (connection) => {
     }
 };
 
-const queryCaseDetail = async (connection, caseId='500Kj00001ZwOx4IAF') => {
+const queryCaseDetail = async (connection, caseNumber) => {
     try {
         //Get the UserId
         const currentuser = await connection.identity();
@@ -24,7 +24,7 @@ const queryCaseDetail = async (connection, caseId='500Kj00001ZwOx4IAF') => {
         // Query for travel requests
         const result = await connection.query(
             //`SELECT Id, Cost__c, Description__c, Destination__c, End_Date__c, Origin__c, Start_Date__c, Status__c, Owner.Name, Name FROM Travel_Request__c WHERE Approver__c = \'${currentuser.user_id}\' AND Status__c = 'New' ORDER BY Name`
-            `SELECT Id, CaseNumber, Status, Subject, Priority, CreatedDate, Type, Account.Name FROM Case WHERE id='${caseId}'`
+            `SELECT Id, CaseNumber, Status, Subject, Priority, CreatedDate, Type, Account.Name FROM Case WHERE CaseNumber='${caseNumber}'`
         );
         return result;
     } catch (e) {
@@ -48,7 +48,7 @@ const queryCaseCarePlans = async (connection) => {
     }
 };
 
-const queryCaseInjuredWorker = async (connection, caseId='500Kj00001ZwOx4IAF') => {
+const queryCaseInjuredWorker = async (connection, caseNumber) => {
     try {
         //Get the UserId
         const currentuser = await connection.identity();
@@ -56,7 +56,7 @@ const queryCaseInjuredWorker = async (connection, caseId='500Kj00001ZwOx4IAF') =
         // Query for travel requests
         const result = await connection.query(
             //`SELECT Id, Cost__c, Description__c, Destination__c, End_Date__c, Origin__c, Start_Date__c, Status__c, Owner.Name, Name FROM Travel_Request__c WHERE Approver__c = \'${currentuser.user_id}\' AND Status__c = 'New' ORDER BY Name`
-            `SELECT Id, Contact.Name, Account.Name, ContactEmail, ContactPhone, ContactMobile FROM Case WHERE id='${caseId}'`
+            `SELECT Id, Contact.Name, Account.Name, ContactEmail, ContactPhone, ContactMobile FROM Case WHERE CaseNumber='${caseNumber}'`
         );
         return result;
     } catch (e) {
@@ -72,7 +72,7 @@ const queryClaims = async (connection, caseId='500Kj00001aT9qdIAC') => {
         // Query for travel requests
         const result = await connection.query(
             //`SELECT Id, Cost__c, Description__c, Destination__c, End_Date__c, Origin__c, Start_Date__c, Status__c, Owner.Name, Name FROM Travel_Request__c WHERE Approver__c = \'${currentuser.user_id}\' AND Status__c = 'New' ORDER BY Name`
-            `SELECT Id, Case__r.caseNumber, Employer_Account__r.Name, Injured_Worker__r.Name, Injury_Details__c, Injury_Type__c, CreateDate FROM Lodgement_Claim__c WHERE Case__c='${caseId}'`
+            `SELECT Id, Case__r.caseNumber, Employer_Account__r.Name, Injured_Worker__r.Name, Injury_Details__c, Injury_Type__c, CreateDate FROM Lodgement_Claim__c WHERE Case__r.caseNumber='${CaseNumber}'`
         );
         return result;
     } catch (e) {

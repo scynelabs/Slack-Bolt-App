e2ee5e0022a674fbcb18ac658662bd4579039bb1
Muhammad Bilal X Nazir <bilal.nazir@scyne.com.au>
2024-07-08 17:46:37 +1000

    commit dialog

--- listeners/index.js
+++ listeners/index.js
@@ -4,10 +4,11 @@ const shortcutsListener = require('./shortcuts');
 const eventsListener = require('./events');
 const actionListener = require('./actions');
 const commandsListener = require('./commands');
+const viewsListener = require('./views');
 
 module.exports.registerListeners = (app) => {
     shortcutsListener.register(app);
     eventsListener.register(app);
     actionListener.register(app);
-    commandsListener.register(app);
+    viewsListener.register(app);
 };
---
+++ listeners/views/index.js
@@ -0,0 +1,46 @@
+
+
+// Handle a view_submission request
+const addCaseNotes = async ({ ack, body, view, client, context, logger }) => {
+    // Acknowledge the view_submission request
+    await ack();
+
+    logger.info('User submitted dialog ==>')
+    logger.info(view.state)
+  
+    /*
+    // Do whatever you want with the input data - here we're saving it to a DB then sending the user a verification of their submission
+  
+    // Assume there's an input block with `block_1` as the block_id and `input_a`
+    const val = view['state']['values']['block_1']['input_a'];
+    const user = body['user']['id'];
+  
+    // Message to send user
+    let msg = '';
+    // Save to DB
+    const results = await db.set(user.input, val);
+  
+    if (results) {
+      // DB save was successful
+      msg = 'Your submission was successful';
+    } else {
+      msg = 'There was an error with your submission';
+    }
+  
+    // Message the user
+    try {
+      await client.chat.postMessage({
+        channel: user,
+        text: msg
+      });
+    }
+    catch (error) {
+      logger.error(error);
+    }
+    */
+  
+  };
+
+  module.exports.register = (app) => {
+    app.view('add_notes_files', addCaseNotes);
+};

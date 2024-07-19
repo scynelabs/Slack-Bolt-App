e1fa1378ee8b6fa72505e0675d83e03d8aa3861a
Muhammad Bilal X Nazir <bilal.nazir@scyne.com.au>
2024-07-19 12:29:48 +1000

    add file_shared event

--- listeners/events/index.js
+++ listeners/events/index.js
@@ -4,4 +4,16 @@ const { appHomeOpenedCallback } = require('./app-home-opened');
 
 module.exports.register = (app) => {
     app.event('app_home_opened', appHomeOpenedCallback);
+    app.event('file_shared', async ({ client, event, payload, body, context, logger }) => {
+        console.log('file_shared event ==>');
+
+        console.log('file_shared event: payload ==>');
+        logger.info(payload)
+
+        console.log('file_shared event: event ==>');
+        logger.info(event)
+
+        console.log('file_shared event: body ==>');
+        logger.info(body)        
+    });
 };
